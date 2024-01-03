97593c45cc73a47f7453ab1a566633b71696623c
Bruno Vieira <vieira.es@gmail.com>
2024-01-03 19:01:24 -0300

    fix html

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public')); // Defina a pasta que contém seus arquivos HTML/CSS
+// Configuração para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
